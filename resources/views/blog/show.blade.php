f00293d5b1c428e6fe18b115d1de641f44a5391b
tanish <tanish.afre@gmail.com>
2022-05-03 06:31:42 +0100

    laravel share

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -17,6 +17,18 @@
     <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
         {{ $post->description }}
     </p>
+
+    <div class="container mt-4">
+        <h6>Share this blogpost!</h6>
+        {!! $share = \Share::page($currentURL = url()->current(),'Check out this blog post')->facebook()->twitter()->reddit()->whatsapp();
+        !!}
+    </div>
+
+
 </div>
 
 @endsection 
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha256-4+XzXVhsDmqanXGHaHvgh1gMQKX40OUvDEBTu8JcmNs=" crossorigin="anonymous"></script>
+<script src="{{ asset('js/share.js') }}"></script>
